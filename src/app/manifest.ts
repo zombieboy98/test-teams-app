e6ce789a007e44dfe2f6ed6bd569b16cb06420bc
zombieboy98 <zeekillingmachine98@gmail.com>
2024-03-30 09:35:07 +0800

    test

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,13 +1,10 @@
-import aadManifest from './aad.manifest';
-
 export default function manifest() {
   return {
-    ...aadManifest(),
     $schema:
       'https://developer.microsoft.com/en-us/json-schemas/teams/v1.15/MicrosoftTeams.schema.json',
     manifestVersion: '1.15',
     version: '1.0.0',
-    id: `${process.env.TAB_ENDPOINT}`,
+    id: `${process.env.TEAMS_APP_ID}`,
     packageName: 'com.microsoft.teams.extension',
     developer: {
       name: 'Teams App, Inc.',
@@ -43,8 +40,9 @@ export default function manifest() {
     permissions: ['identity', 'messageTeamMembers'],
     validDomains: [`${process.env.TAB_ENDPOINT}`],
     webApplicationInfo: {
-      id: `${process.env.AAD_APP_CLIENT_ID}`,
-      resource: `api://${process.env.TAB_DOMAIN}/${process.env.AAD_APP_CLIENT_ID}`,
+      id: 'f79a3e63-32d8-4c5b-8fb4-6489061197c5',
+      resource:
+        'api://customer-insights.macquariecloudservices.com/f79a3e63-32d8-4c5b-8fb4-6489061197c5',
     },
   };
 }
